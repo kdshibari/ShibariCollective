7d1b0a8de3ca73cc6daeb1e74d1e91505b384d12
tkm <thekinkymap@gmail.com>
2026-07-27 19:56:37 +0700

    Update Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,14 +2,6 @@ import { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import { Link } from "@tanstack/react-router";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-});
 
 function MapUpdater({ center }: { center: [number, number] }) {
   const map = useMap();
@@ -20,6 +12,17 @@ function MapUpdater({ center }: { center: [number, number] }) {
 }
 
 export default function Map({ userLoc, filtered }: { userLoc: any, filtered: any[] }) {
+  useEffect(() => {
+    import("leaflet").then((L) => {
+      delete (L.Icon.Default.prototype as any)._getIconUrl;
+      L.Icon.Default.mergeOptions({
+        iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+        iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+        shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+      });
+    });
+  }, []);
+
   return (
     <MapContainer
       center={userLoc ? [userLoc.lat, userLoc.lng] : [20, 0]}
